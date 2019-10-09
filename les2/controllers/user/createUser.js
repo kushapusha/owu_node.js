d77097c745ee6a2471c6f49da156169d31913741
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 17:59:18 +0300

    Update createUser.js

--- les2/controllers/user/createUser.js
+++ les2/controllers/user/createUser.js
@@ -2,9 +2,9 @@ const { provider } = require('../../database');
 
 module.exports = async (req, res) => {
     const {name, email, password} = req.body;
-    const query = `insert into users(name, email, password) values (?, ?, ?)`;
+    const query = `insert into users(name, email, password) values ('${name}','${email}','${password}')`;
 
-    await provider.promise().query(query, [name, email, password]);
+    await provider.promise().query(query);
 
     res.render('regist');
 };
