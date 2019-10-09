4747933d4435eb3ef4cd4cdeaca437f5dcfdcda8
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 17:56:28 +0300

    Update createHouse.js

--- les2/controllers/house/createHouse.js
+++ les2/controllers/house/createHouse.js
@@ -2,9 +2,9 @@ const { provider } = require('../../database');
 
 module.exports = async (req, res) => {
     const {city, street, rooms} = req.body;
-    const query = `insert into house(city, street, rooms) values (?, ?, ?)`;
+    const query = `insert into house(city, street, rooms) values ('${city}', '${street}', '${rooms}')`;
 
-    await provider.promise().query(query, [city, street, rooms]);
+    await provider.promise().query(query);
 
     res.render('house');
 };
