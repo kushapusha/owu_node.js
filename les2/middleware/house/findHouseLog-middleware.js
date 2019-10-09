dc9848274e203e68afec7482bae77bf4c8807f1e
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 18:01:36 +0300

    Update findHouseLog-middleware.js

--- les2/middleware/house/findHouseLog-middleware.js
+++ les2/middleware/house/findHouseLog-middleware.js
@@ -4,13 +4,14 @@ module.exports = async (req, res , next) => {
     try {
         const {street} = req.body;
         const query = `select * from house where street = '${street}'`;
+        
         const [LoginHouse] = await provider.promise().query(query);
 
         if (!LoginHouse.length) {
             return res.redirect('/house');
         }
 
-        [req.house] = LoginHouse;
+        req.house = LoginHouse;
         next();
     } catch (e) {
         res.status(400).json(e.message)
