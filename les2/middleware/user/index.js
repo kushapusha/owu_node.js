c04101a3fd161518595522f1e85125f51a689d9b
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 18:05:31 +0300

    Update index.js

--- les2/middleware/user/index.js
+++ les2/middleware/user/index.js
@@ -2,11 +2,13 @@ const isUserPresentMiddleware = require('./isUserPresent-middleware');
 const checkUserValidMiddleware = require('./checkUserValid-middleware');
 const findUserLogMiddleware = require('./findUserLog-middleware');
 const isIDinDbPresentMiddleware = require('./isIDinDbPresent-middleware');
+const findAllUsersMiddleware = require('./findAllUsers-middleware');
 
 
 module.exports = {
     isUserPresentMiddleware,
     checkUserValidMiddleware,
     findUserLogMiddleware,
-    isIDinDbPresentMiddleware
+    isIDinDbPresentMiddleware,
+    findAllUsersMiddleware
 };
