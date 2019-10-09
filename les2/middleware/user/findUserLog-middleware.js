7d498a0a9b43fc9fa1c06a247e4e67ee5903eac5
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 18:04:01 +0300

    Update findUserLog-middleware.js

--- les2/middleware/user/findUserLog-middleware.js
+++ les2/middleware/user/findUserLog-middleware.js
@@ -4,13 +4,14 @@ module.exports = async (req, res ,next) => {
     try {
         const {email, password} = req.body;
         const query = `select * from users where email = '${email}' and password = '${password}'`;
+        
         const [LoginUser] = await provider.promise().query(query);
 
         if (!LoginUser.length) {
             return res.redirect('/regist');
         }
 
-        [req.user] = LoginUser;
+        req.user = LoginUser;
         next();
     } catch (e) {
         res.status(400).json(e.message)
