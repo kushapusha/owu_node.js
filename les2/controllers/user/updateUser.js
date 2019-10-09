8b5ccc14d637b6547bf2848c13487ef72e5f5ebe
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 18:00:47 +0300

    Update updateUser.js

--- les2/controllers/user/updateUser.js
+++ les2/controllers/user/updateUser.js
@@ -4,6 +4,7 @@ module.exports = async (req, res) => {
 
     const {id, name, email, password} = req.body;
     const query = `update users set name = '${name}', email = '${email}', password = '${password}' where id = ${id}`;
+    
     await provider.promise().query(query);
 
     res.redirect('/userUpdate');
