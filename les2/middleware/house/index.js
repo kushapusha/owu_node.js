8a08885b33bd40f896815e0669b5b559d149514d
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-09 18:03:33 +0300

    Update index.js

--- les2/middleware/house/index.js
+++ les2/middleware/house/index.js
@@ -2,11 +2,14 @@ const isHousePresentMiddleware = require('./isHousePresent-middleware');
 const checkHouseValidMiddleware = require('./checkHouseValid-middleware');
 const findHouseLogMiddleware = require('./findHouseLog-middleware');
 const isHouseIdInDbPresentMiddleware = require('./isHouseIdInDbPresent-middleware');
+const findAllHousesMiddleware = require('./findAllHouses-middleware');
+
 
 
 module.exports = {
     isHousePresentMiddleware,
     checkHouseValidMiddleware,
     findHouseLogMiddleware,
-    isHouseIdInDbPresentMiddleware
+    isHouseIdInDbPresentMiddleware,
+    findAllHousesMiddleware
 };
