1aa44d871a1be2fd04d6581e5bac51f97d5b2166
Vika Kushnir <54114713+kushapusha@users.noreply.github.com>
2019-10-12 23:21:20 +0300

    delete updating pages

--- les3/controllers/workPages/index.js
+++ les3/controllers/workPages/index.js
@@ -2,15 +2,10 @@ const mainPage = require('./mainPage');
 const loginPage = require('./loginPage');
 const registPage = require('./registPage');
 const housePage = require('./housePage');
-const userUpdatingPage = require('./userUpdatingPage');
-const houseUpdatingPage = require('./houseUpdatingPage');
-
 
 module.exports = {
     mainPage,
     loginPage,
     registPage,
     housePage,
-    userUpdatingPage,
-    houseUpdatingPage
-}
+};
